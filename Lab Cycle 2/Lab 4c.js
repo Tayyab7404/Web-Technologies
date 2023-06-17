60bf8b2298d7e7c83d1a1b56cadc7560a5dd631c
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2023-06-17 09:19:02 +0530

    Add files via upload

--- Lab Cycle 2/Lab 4c.js
+++ Lab Cycle 2/Lab 4c.js
@@ -1,12 +1,12 @@
 // Matrix Addition and Multiplication:
 
 const mat1 = [[1,2,3],
-			  [4,5,6],
-			  [7,8,9]];
+	      [4,5,6],
+	      [7,8,9]];
 
 const mat2 = [[10,11,12],
-			  [13,14,15],
-			  [16,17,18]]
+	      [13,14,15],
+	      [16,17,18]]
 
 function showMatrices()
 {
---
+++ Lab Cycle 2/Lab 6b.js
@@ -0,0 +1,6 @@
+function Click()
+{
+    let Output = document.getElementById("OnClickText");
+    
+    Output.innerHTML = "You Clicked the Button!"
+}
