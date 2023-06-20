3003891fc886ad0cd310a5e6a75449d6090881eb
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2023-06-20 14:36:09 +0530

    Add files via upload

--- Lab Cycle 2/Lab 6a.js
+++ Lab Cycle 2/Lab 6a.js
@@ -5,27 +5,35 @@ function NameChange()
     // Changed = Alerts the user that Name is changed
     window.alert("Name is changed.")
 }
-function OnFocus() 
+function NameFocus() 
 {
     // Focus = Changes the background color of input to yellow
-    document.getElementById("Name").style.background = "yellow";
+    Name.style.background = "yellow";
 }
-function OnBlur() 
+function NameBlur() 
 {
     // No focus = Changes the background color of input to orange
-    document.getElementById("Name").style.background = "orange";
+    Name.style.background = "orange";
 }
+function RegdnoFocus() 
+{ Regdno.style.background = "yellow"; }
+function RegdnoBlur() 
+{ Regdno.style.background = "orange"; }
+function SectionFocus() 
+{ Section.style.background = "yellow"; }
+function SectionBlur() 
+{ Section.style.background = "orange"; }
 
 Name = document.getElementById("Name");
 Regdno = document.getElementById("Regdno");
 Section = document.getElementById("Section");
 
-Name.addEventListener('onchange', NameChange);
-Name.addEventListener('onfocus', OnFocus);
-Name.addEventListener('onblur', OnBlur);
+Name.addEventListener('change', NameChange);
+Name.addEventListener('focus', NameFocus);
+Name.addEventListener('blur', NameBlur);
 
-Regdno.addEventListener('onfocus', OnFocus);
-Regdno.addEventListener('onblur', OnBlur);
+Regdno.addEventListener('focus', RegdnoFocus);
+Regdno.addEventListener('blur', RegdnoBlur);
 
-Section.addEventListener('onfocus', OnFocus);
-Section.addEventListener('onblur', OnBlur);
+Section.addEventListener('focus', SectionFocus);
+Section.addEventListener('blur', SectionBlur);
