1c3bc022518ea278a6b28a5a3e048bdfd8d317ff
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2023-06-20 12:56:50 +0530

    Add files via upload

--- Lab Cycle 2/Lab 6b.js
+++ Lab Cycle 2/Lab 6b.js
@@ -4,3 +4,41 @@ function Click()
     
     Output.innerHTML = "You Clicked the Button!"
 }
+
+function MouseDown()
+{
+    let text = document.getElementById("OnMouseDownText")
+
+    text.style.color = 'red';
+}
+
+function MouseUp()
+{
+    let text = document.getElementById("OnMouseDownText")
+
+    text.style.color = 'black';
+}
+
+function MouseEnter(x)
+{
+    x.style.width = '80px';
+    x.style.height = '64px';
+}
+
+function MouseLeave(x)
+{
+    x.style.width = '40px';
+    x.style.height = '32px';
+}
+
+function MouseOver()
+{
+    let text = document.getElementById("MouseOverText");
+    text.style.color = 'red';
+}
+
+function MouseOut()
+{
+    let text = document.getElementById("MouseOverText");
+    text.style.color = 'black';
+}
