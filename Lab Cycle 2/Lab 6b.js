7741ae7d36cd0c4f083ccced6b8da673eaf31c7a
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2023-06-20 13:44:47 +0530

    Add files via upload

--- Lab Cycle 2/Lab 6b.js
+++ Lab Cycle 2/Lab 6b.js
@@ -1,44 +1,51 @@
 function Click()
 {
-    let Output = document.getElementById("OnClickText");
-    
-    Output.innerHTML = "You Clicked the Button!"
+    document.getElementById("OnClickText").innerHTML = "You Clicked the Button!"
 }
 
 function MouseDown()
 {
-    let text = document.getElementById("OnMouseDownText")
-
-    text.style.color = 'red';
+    OnMouseDownText.style.color = 'red';
 }
 
 function MouseUp()
 {
-    let text = document.getElementById("OnMouseDownText")
-
-    text.style.color = 'black';
+    OnMouseDownText.style.color = 'black';
 }
 
-function MouseEnter(x)
+function MouseEnter()
 {
-    x.style.width = '80px';
-    x.style.height = '64px';
+    image.style.width = '80px';
+    image.style.height = '64px';
 }
 
-function MouseLeave(x)
+function MouseLeave()
 {
-    x.style.width = '40px';
-    x.style.height = '32px';
+    image.style.width = '40px';
+    image.style.height = '32px';
 }
 
 function MouseOver()
 {
-    let text = document.getElementById("MouseOverText");
-    text.style.color = 'red';
+    MouseOverText.style.color = 'red';
 }
 
 function MouseOut()
 {
-    let text = document.getElementById("MouseOverText");
-    text.style.color = 'black';
+    MouseOverText.style.color = 'black';
 }
+
+let OnClickButton = document.getElementById('OnClickButton');
+OnClickButton.addEventListener('click', Click);
+
+let OnMouseDownText = document.getElementById("OnMouseDownText");
+OnMouseDownText.addEventListener('mousedown', MouseDown);
+OnMouseDownText.addEventListener('mouseup', MouseUp);
+
+let image = document.getElementById("emoji");
+image.addEventListener('mouseenter', MouseEnter);
+image.addEventListener('mouseleave', MouseLeave)
+
+let MouseOverText = document.getElementById("MouseOverText");
+MouseOverText.addEventListener('mouseover',MouseOver);
+MouseOverText.addEventListener('mouseout',MouseOut);
