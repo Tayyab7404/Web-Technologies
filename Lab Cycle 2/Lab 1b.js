c1f189acd08871143c7918c9580203876f510c76
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2023-06-23 20:21:26 +0530

    Update Lab 1b.js

--- Lab Cycle 2/Lab 1b.js
+++ Lab Cycle 2/Lab 1b.js
@@ -2,28 +2,28 @@
 
 function largestAmongThree(a, b, c) 
 {
-  	let max = a;
+    let max = a;
 
-	if(b > max)
-		max = b;
-	if(c > max)
-		max = c;
+    if(b > max)
+        max = b;
+    if(c > max)
+        max = c;
 
-	return max;
+    return max;
 }
 
 function getNum()
 {
-	let n1 = parseFloat(document.getElementById("num1").value);
-	let n2 = parseFloat(document.getElementById("num2").value);
-	let n3 = parseFloat(document.getElementById("num3").value);
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let n3 = parseFloat(document.getElementById("num3").value);
 
-	let answer = '';
+    let answer = '';
 
-	if(Math.floor(n1%1) <= 0 && Math.floor(n2%1) <= 0 && Math.floor(n3%1) <= 0)
-		answer = "Largest Number Among "+n1+", "+n2+", "+n3+" = "+largestAmongThree(n1,n2,n3);
-	else
-		answer = "Invalid Number!";
-	
-	document.getElementById("output").innerHTML = answer;
+    if(Math.floor(n1%1) <= 0 && Math.floor(n2%1) <= 0 && Math.floor(n3%1) <= 0)
+	answer = "Largest Number Among "+n1+", "+n2+", "+n3+" = "+largestAmongThree(n1,n2,n3);
+    else
+	answer = "Invalid Number!";
+
+    document.getElementById("output").innerHTML = answer;
 }
