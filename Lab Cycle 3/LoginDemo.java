5cbc23e98af80b4df46a5b98282612a87ec21308
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2023-07-01 16:28:22 +0530

    Create LoginDemo.java

---
+++ Lab Cycle 3/LoginDemo.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package p1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author y21cs172
+ */
+public class LoginDemo extends HttpServlet {
+
+   public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+      // Set response content type
+      response.setContentType("text/html");
+
+      // Actual logic goes here.
+      PrintWriter out = response.getWriter();
+      String name=request.getParameter("username");//will return value
+      String pwd=request.getParameter("pass");
+	  //out.println("Welcome "+name);
+      if(name.equalsIgnoreCase("Tayyab")&&pwd.equalsIgnoreCase("12345"))
+          out.println("Login Successful! "+name);
+      else
+          out.println("Login Unsucceusful! "+name);
+	  out.close();
+   }
+}
